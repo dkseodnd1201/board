c8effcc2127c326fa6a16914c2274701afde88af
andaewoong <dkseodnd1201@gmail.com>
2021-12-06 19:55:45 +0900

    2021-12-06 임시

--- src/main/java/woongzzi/board/controller/MemberController.java
+++ src/main/java/woongzzi/board/controller/MemberController.java
@@ -2,19 +2,17 @@ package woongzzi.board.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import woongzzi.board.DTO.MemberDTO;
 import woongzzi.board.service.MemberService;
 
 @Controller
 @AllArgsConstructor
+@RequestMapping(value = "/member")
 public class MemberController {
     private MemberService memberService;
     //로그인 페이지
-    @GetMapping("/")
+    @GetMapping("/login")
     public String login() {
         return "/member/login";
     }
@@ -28,7 +26,7 @@ public class MemberController {
     @PostMapping("/register")
     public String registerPost(MemberDTO memberDTO) {
         memberService.joinUser(memberDTO);
-        return "redirect:/board/list";
+        return "redirect:/member/login";
     }
 
     //비밀번호 분실 페이지
--- src/main/java/woongzzi/board/controller/config/SecurityConfig.java
+++ src/main/java/woongzzi/board/controller/config/SecurityConfig.java
@@ -14,9 +14,9 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import woongzzi.board.service.MemberService;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity //Spring Security 설정할 클래스 정의
 @AllArgsConstructor
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter { //WebSecurityConfigurer 인스턴스를 편리하게 생성하기 위한 클래스
     private MemberService memberService;
 
     @Bean
@@ -29,20 +29,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception //WebSecurity : FilterChainProxy를 생성하는 필터
     {
         // static 디렉터리의 하위 파일 목록은 인증 무시 ( = 항상통과 )
+        // resource/static 기준
         web.ignoring().antMatchers("/css/**", "/js/**", "/img/**", "/lib/**");
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                // 페이지 권한 설정
+    protected void configure(HttpSecurity http) throws Exception { //HttpSecurity를 통해 HTTP 요청에 대한 웹 기반 보안을 구성
+        http.authorizeRequests() //HttpServletRequest에 따라 접근(access)을 제한
+//                 페이지 권한 설정 왼쪽 uri는 지정된 Role만 접근 가능
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/user/myinfo").hasRole("MEMBER")
                 .antMatchers("/").permitAll()
+                //.anyRequest().authenticated() 인증된 사용자만 접근 가능
                 .and() // 로그인 설정
                 .formLogin()
-                .loginPage("/user/login")
-                .defaultSuccessUrl("/user/login/result")
+                .loginPage("/member/login")
+                .defaultSuccessUrl("/board/list")
+                .usernameParameter("id")
+                .passwordParameter("password")
                 .permitAll()
                 .and() // 로그아웃 설정
                 .logout()
